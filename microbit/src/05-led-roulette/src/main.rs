162f1d0cea67593621ba9f0dc93f0e0dc6a56d0e
Ivan Vukman <ivan.vukman@gmail.com>
2023-05-04 23:20:37 +0200

    Implement section 5 challenge

--- microbit/src/05-led-roulette/src/main.rs
+++ microbit/src/05-led-roulette/src/main.rs
@@ -3,15 +3,52 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use panic_halt as _;
-use microbit as _;
+use rtt_target::rtt_init_print;
+use panic_rtt_target as _;
+use microbit::{
+    board::Board,
+    display::blocking::Display,
+    hal::{prelude::*, Timer},
+};
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    rtt_init_print!();
 
-    // infinite loop; just so we don't leave this stack frame
-    loop {}
+    let board = Board::take().unwrap();
+    let mut timer = Timer::new(board.TIMER0);
+    let mut display = Display::new(board.display_pins);
+    let mut matrix = [
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0],
+    ];
+
+    loop {
+        for i in 1..5 {
+            matrix[0][i - 1] = 0;
+            matrix[0][i] = 1;
+            display.show(&mut timer, matrix, 50);
+        }
+
+        for i in 1..5 {
+            matrix[i - 1][4] = 0;
+            matrix[i][4] = 1;
+            display.show(&mut timer, matrix, 50);
+        }
+
+        for i in (1..5).rev() {
+            matrix[4][i] = 0;
+            matrix[4][i - 1] = 1;
+            display.show(&mut timer, matrix, 50);
+        }
+
+        for i in (1..5).rev() {
+            matrix[i][0] = 0;
+            matrix[i - 1][0] = 1;
+            display.show(&mut timer, matrix, 50);
+        }
+    }
 }
